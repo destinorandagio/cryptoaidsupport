950afa4be86ac9d9f7540f36d6c94823d4a33780
Destino Randagio <destinorandagio@gmail.com>
2026-09-02 11:27:33 +0200

    feat(chat03): bootstrap digital twin search wallet dappmap

---
+++ twin/__init__.py
@@ -0,0 +1,5 @@
+"""CryptoAID Digital Twin, Search, Wallet and DAPPMAP read-model package."""
+
+from .engine import DigitalTwinEngine, TwinRecord, TwinStatus
+
+__all__ = ["DigitalTwinEngine", "TwinRecord", "TwinStatus"]
